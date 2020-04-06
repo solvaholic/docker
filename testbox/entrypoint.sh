15d4ebe78173f6969291eff60d410d17fb3d4e45
Roger D. Winans <solvaholic@users.noreply.github.com>
2020-04-05 22:39:39 -0400

    Add gh to testbox

--- testbox/entrypoint.sh
+++ testbox/entrypoint.sh
@@ -4,6 +4,7 @@
 
 # Requirements:
 #   - /github/workspace or /myrepo contains a clone of the repository to test
+#   - If you pass GITHUB_TOKEN and GITHUB_USER, gh will use them.
 
 # If /github/workspace is not a Git repository, try /myrepo. If that's not,
 # either, then fail.
@@ -16,11 +17,24 @@ else
   exit
 fi
 
-echo "INFO: Found a repository in ${_myrepo}."
+echo "INFO: Found a Git repository in ${_myrepo}."
+
+# If there's not already a gh config, and if GITHUB_USER and GITHUB_TOKEN are
+# set, configure gh credentials.
+_ghcfg=~/.config/gh/config.yml
+if [ ! -f "${_ghcfg}" ] && [ -n "${GITHUB_USER}" ] && [ -n "${GITHUB_TOKEN}" ]
+then
+  echo "INFO: GITHUB_USER is set to '${GITHUB_USER}'."
+  echo "INFO: GITHUB_TOKEN is set, too. Configuring gh."
+  mkdir -p "$(dirname "$_ghcfg")"
+  echo "github.com:" > "${_ghcfg}"
+  echo "- user: ${GITHUB_USER}" >> "${_ghcfg}"
+  echo "  oauth_token: ${GITHUB_TOKEN}" >> "${_ghcfg}"
+fi
 
 # If $_myrepo/script/test is not executable, fail.
 [ -x "${_myrepo}/script/test" ] || exit
 
 echo "INFO: Found script/test. Running it."
 
-"${_myrepo}/script/test"
+"${_myrepo}/script/test" || exit
