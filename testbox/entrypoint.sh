4088b7af17f50afdd77609c01d4198050ec933ac
Roger D. Winans <solvaholic@users.noreply.github.com>
2021-01-18 15:11:17 -0500

    Remove gh config to its own script

--- testbox/entrypoint.sh
+++ testbox/entrypoint.sh
@@ -6,37 +6,17 @@
 #   - /github/workspace or /myrepo contains a clone of the repository to test
 #   - If GITHUB_TOKEN and GITHUB_USER are set, gh will use them.
 
-# If /github/workspace is not a Git repository, try /myrepo. If that's not,
-# either, then fail.
+# If /github/workspace is not a Git repository, try /myrepo.
 echo "INFO: Checking for a repository in /github/workspace, or /myrepo."
 if git rev-parse --resolve-git-dir /github/workspace/.git >/dev/null; then
   _myrepo=/github/workspace
 elif git rev-parse --resolve-git-dir /myrepo/.git >/dev/null; then
   _myrepo=/myrepo
 else
-  exit
+  echo "      ... None found."
+  _myrepo=""
 fi
 
-echo "INFO: Found a Git repository in ${_myrepo}."
+[ -n "${_myrepo}" ] && echo "INFO: Found a Git repository in ${_myrepo}."
 
-# If there's not already a gh config, and if GITHUB_USER and GITHUB_TOKEN are
-# set, configure gh credentials.
-_ghcfg=~/.config/gh/config.yml
-[ -z "${GITHUB_TOKEN}" ] && GITHUB_TOKEN="${INPUT_GITHUB_TOKEN:-""}"
-[ -z "${GITHUB_USER}" ] && GITHUB_USER="${INPUT_GITHUB_USER:-""}"
-if [ ! -f "${_ghcfg}" ] && [ -n "${GITHUB_USER}" ] && [ -n "${GITHUB_TOKEN}" ]
-then
-  echo "INFO: GITHUB_USER is set to '${GITHUB_USER}'."
-  echo "INFO: GITHUB_TOKEN is set, too. Configuring gh."
-  mkdir -p "$(dirname "$_ghcfg")"
-  echo "github.com:" > "${_ghcfg}"
-  echo "- user: ${GITHUB_USER}" >> "${_ghcfg}"
-  echo "  oauth_token: ${GITHUB_TOKEN}" >> "${_ghcfg}"
-fi
-
-# If $_myrepo/script/test is not executable, fail.
-[ -x "${_myrepo}/script/test" ] || exit
-
-echo "INFO: Found script/test. Running it."
-
-"${_myrepo}/script/test" || exit
+/bin/bash
